5979f4dbc622f61777491523de7753257d69d42b
Uthaeus <romanlavery@gmail.com>
2023-04-01 15:02:56 -0600

    slightly refactored tweets controller, cleaned up tweets view folder

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -23,38 +23,28 @@ class TweetsController < ApplicationController
   def create
     @tweet = Tweet.new(tweet_params)
 
-    respond_to do |format|
-      if @tweet.save
-        format.html { redirect_to tweet_url(@tweet), notice: "Tweet was successfully created." }
-        format.json { render :show, status: :created, location: @tweet }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
-      end
+    if @tweet.save
+        redirect_to tweet_url(@tweet), notice: "Tweet was successfully created."
+    else
+        render :new, status: :unprocessable_entity 
     end
   end
 
   # PATCH/PUT /tweets/1 or /tweets/1.json
   def update
-    respond_to do |format|
-      if @tweet.update(tweet_params)
-        format.html { redirect_to tweet_url(@tweet), notice: "Tweet was successfully updated." }
-        format.json { render :show, status: :ok, location: @tweet }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @tweet.errors, status: :unprocessable_entity }
-      end
+    
+    if @tweet.update(tweet_params)
+      redirect_to tweet_url(@tweet), notice: "Tweet was successfully updated." 
+    else
+      render :edit, status: :unprocessable_entity 
     end
   end
 
   # DELETE /tweets/1 or /tweets/1.json
   def destroy
     @tweet.destroy
-
-    respond_to do |format|
-      format.html { redirect_to tweets_url, notice: "Tweet was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    
+    redirect_to tweets_url, notice: "Tweet was successfully destroyed." 
   end
 
   private
